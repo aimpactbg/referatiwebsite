ade1e0c2d73a62d34f97d0271378a468e2a7ef91
aimpactbg <aimpactbg@gmail.com>
2025-07-31 21:06:28 +0000

    feat: integrate GTM with environment variable

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="bg">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+    return (
+      <Html lang="bg">
+        <Head />
+        <body>
+          {gtmId && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+              ></iframe>
+            </noscript>
+          )}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
